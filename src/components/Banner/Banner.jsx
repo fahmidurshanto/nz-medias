e04ccce16fe5db1f272eb2df4fcd457f9a734f55
Fahmidur Rahaman Shanto <fahmidurrahamanshanto@gmail.com>
2024-03-04 02:42:37 +0600

    added type writter animation effect to the banner title

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,61 +1,25 @@
+import { TypeAnimation } from "react-type-animation";
 import BannerBody from "../BannerBody/BannerBody";
 
 // Designed by using AOS  library, the website is divided into three sections:
 const Banner = () => {
   return (
-    <div data-aos="zoom-in" className="bg-[rgb(225,224,245)]">
-      <h3 className="p-10 text-7xl text-center bg-[#C4C1EB]">
-        Welcome to{" "}
-        <span
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-          className="text-red-500"
-        >
-          N
-        </span>
-        <span
-          className="text-red-500"
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-        >
-          Z
-        </span>
-        <span
-          className="text-red-500"
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-        >
-          {" "}
-          M
-        </span>
-        <span
-          className="text-red-500"
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-        >
-          E
-        </span>
-        <span
-          className="text-red-500"
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-        >
-          D
-        </span>
-        <span
-          className="text-red-500"
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-        >
-          I
-        </span>
-        <span
-          className="text-red-500"
-          data-aos="zoom-in-down"
-          data-aos-duration="5000"
-        >
-          A
-        </span>
+    <div data-aos="zoom-in" className="bg-[rgb(225,224,245)] container mx-auto">
+      {/* className="p-10 text-7xl text-center bg-[#C4C1EB] */}
+      <h3 className="p-10 text-6xl text-center bg-[#C4C1EB]">
+        WELCOME TO <br />
+        <TypeAnimation
+          className="text-red-400 font-semibold"
+          sequence={[
+            // Same substring at the start will only be typed out once, initially
+            ` NZ MEDIA`,
+            5000, // wait 1s before replacing "Mice" with "Hamsters"
+          ]}
+          wrapper="span"
+          speed={50}
+          style={{ fontSize: "2em", display: "inline-block" }}
+          repeat={Infinity}
+        />
       </h3>
       <BannerBody />
     </div>
